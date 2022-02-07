c97ca5aae3eda0716acdf7b217286bbd3312229e
Arno Messiaen <arnomessiaen@gmail.com>
2022-02-07 23:51:38 +0100

    Add AMTECH rpi3 router board
    (cherry picked from commit 56eb6882df5824d23a3dc2176a54cb4285158548)

---
+++ board/amtech/rpi3_router/post-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+GENIMAGE_CFG="board/amtech/rpi3_router/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
+# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
+# space. We don't rely on genimage to build the rootfs image, just to insert a
+# pre-built one in the disk image.
+
+trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+ROOTPATH_TMP="$(mktemp -d)"
+
+rm -rf "${GENIMAGE_TMP}"
+
+cp board/amtech/rpi3_router/cmdline.txt "${BINARIES_DIR}/rpi-firmware/"
+
+genimage \
+	--rootpath "${ROOTPATH_TMP}"   \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
